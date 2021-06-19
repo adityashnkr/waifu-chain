07c4ed0605c17119b405a11f3a544004e9fca194
adityashnkr <adityashankar009@gmail.com>
2021-06-19 18:46:40 +0530

    added waifu struct

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,16 +1,22 @@
 pragma solidity >=0.4.16 <0.9.0;
 
 contract Election {
-    // struct Waifu {
-    //     uint256 id;
-    //     string name;
-    //     uint256 voteCount;
-    // }
-    // mapping(uint256 => Waifu) public waifus;
-    // uint256 public waifuCount;
+    struct Waifu {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+    mapping(uint256 => Waifu) public waifus;
+    uint256 public waifuCount;
     string public waifu;
 
     constructor() public {
-        waifu = "waifu 1";
+        addWaifu("Waifu 1");
+        addWaifu("Waifu 2");
+    }
+
+    function addWaifu(string memory _name) private {
+        waifuCount++;
+        waifus[waifuCount] = Waifu(waifuCount, _name, 0);
     }
 }
